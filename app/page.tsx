0ddfafa1a532302e822eb3381313c9da4cffc62c
Devanand Vijayathilakan <devanandv12@gmail.com>
2023-10-05 16:25:48 +0100

    Added ssearch bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width " id='discover'>
+        <div className="home__text-container">
+          <h1 className='text-4xl font-extrabold'>Vehicle Catalogue</h1>
+          <p>Explore the e-vehicles you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='country' />
+            <CustomFilter title='year' />
+          </div>
+        </div>
+      </div>
     </main>
+    
   )
 }
